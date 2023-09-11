189b1c16c27f98c934355f957846dc776494b16a
Jesus Alberto Diaz Dominguez <albertodiaz.42069@gmail.com>
2023-09-11 12:52:58 -0600

    AddComponets

---
+++ Assets/Scritps/AccessDate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AccessDate : MonoBehaviour
+{
+    private string screenshotTime = System.DateTime.Now.ToString("MM/dd/yy");
+    public Text accessDate;
+    void Update()
+    {
+        GetData();
+    }
+
+    void GetData(){
+        accessDate.text = "Fecha de acceso: " + screenshotTime;
+    }
+}
---
+++ Assets/Scritps/CambioEscenaLogoUjat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscenaLogoUjat : MonoBehaviour
+{
+    public string scena;
+    void Start()
+    {
+        StartCoroutine(Logos());
+    }
+
+    IEnumerator Logos() 
+    {
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene(scena);
+    }
+    
+
+}
